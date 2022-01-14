c874e093b80f79a3f7599ab20d9b62fa4430da2d
BrennoMuller <82506300+BrennoMuller@users.noreply.github.com>
2022-01-14 15:17:53 -0300

    Update main.js
    add open modal
    correção posicionamento dos cards

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 //elementos da dom
 var cards = document.querySelectorAll('.card');
 var backs = document.querySelectorAll('.back');
-
+var text_pontos_jogador1 = document.querySelector(".pontos1");
+var text_pontos_jogador2 = document.querySelector(".pontos2");
+var text_pontos_jogador1 = document.querySelector(".pontos1");
+var text_pontos_jogador2 = document.querySelector(".pontos2");
+var qual_jogador = document.querySelector(".qual_jogador");
 
 ////adicionando um id a todos cards\\\\
 
@@ -66,14 +70,10 @@ var card_virada_2;
 var pontos_jogador1 = 0;
 var pontos_jogador2 = 0;
 var cartas_viradas = 0;
-var text_pontos_jogador1 = document.querySelector(".pontos1");
-var text_pontos_jogador2 = document.querySelector(".pontos2");
-var qual_jogador = document.querySelector(".qual_jogador");
+
 qual_jogador.value = 1;
 
 function flip(div){
-    text_pontos_jogador1.style.animation = '0';
-    text_pontos_jogador2.style.animation = '0';
 
     if(div.classList.length == 2){
         return;
@@ -84,57 +84,103 @@ function flip(div){
         card_virada_1 = div;
         cartas_viradas += 1;
         return;
-    }
-
-    if(cartas_viradas == 1){
+    }else if(cartas_viradas == 1){
         div.classList.toggle('flip');
         card_virada_2 = div;
         cartas_viradas  += 1;
-       
         var card1_id = card_virada_2.children[1].id;
         var card2_id = card_virada_1.children[1].id;
+        comparar_cards(card1_id , card2_id);
+    }else{
+        card_virada_1.classList.toggle('flip');
+        card_virada_2.classList.toggle('flip');
+        cartas_viradas = 0;
+        troca_jogador();
 
-
-        if(card1_id  == card2_id){
-            if(qual_jogador.value == 1){
-                pontos_jogador1 += 1;
-                text_pontos_jogador1.innerHTML = "Pontos: " + pontos_jogador1;
-                text_pontos_jogador1.style.animation = 'pontos 0.5s';
-                cartas_viradas = 0;
-                return;
-            }
+    }
     
-            if(qual_jogador.value == 2){
-                pontos_jogador2 += 1;
-                text_pontos_jogador2.innerHTML = "Pontos: " + pontos_jogador2;
-                text_pontos_jogador2.style.animation = 'pontos 0.5s';
-                cartas_viradas = 0;
-                return;
-            }
+}
 
-        
-        }else{
-            console.log(qual_jogador.value);
-            if(qual_jogador.value == 1){
-                qual_jogador.value = '2';
-                qual_jogador.innerHTML = "jogador 2";
-                setTimeout(desvirar, 1000);
-                cartas_viradas = 0;
-            }else{
-                qual_jogador.value = '1';
-                qual_jogador.innerHTML = "jogador 1";
-                setTimeout(desvirar, 1000);
-                cartas_viradas = 0;
-            }
+
+////comparando as cartas\\\\\
+function comparar_cards(card1_id , card2_id){
+
+    text_pontos_jogador1.style.animation = '0';
+    text_pontos_jogador2.style.animation = '0';
+
+    if(card1_id  == card2_id){
+        if(qual_jogador.value == 1){
+            pontos_jogador1 += 1;
+            text_pontos_jogador1.innerHTML = "acertos jogador 1: " + pontos_jogador1;
+            text_pontos_jogador1.style.animation = 'pontos 0.5s';
+            cartas_viradas = 0;
+            verificando_vencedor();
         }
-    }   
     
-    
-          
+        if(qual_jogador.value == 2){
+            pontos_jogador2 += 1;
+            text_pontos_jogador2.innerHTML = "acertos jogador 1: " + pontos_jogador2;
+            text_pontos_jogador2.style.animation = 'pontos 0.5s';
+            cartas_viradas = 0;
+            verificando_vencedor();
+        }
+    }
 }
 
 
-function desvirar(){
-    card_virada_1.classList.toggle('flip');
-    card_virada_2.classList.toggle('flip');
+
+
+
+////trocando jogador\\\\\
+
+function troca_jogador(){
+    if(qual_jogador.value == 1){
+        qual_jogador.value = '2';
+        qual_jogador.innerHTML = "jogador 2";
+        cartas_viradas = 0;             
+    }else{
+        qual_jogador.value = '1';
+        qual_jogador.innerHTML = "jogador 1";
+        cartas_viradas = 0;    
+    }  
 }
+ 
+
+
+
+
+
+////verificando vencedor\\\\\
+function verificando_vencedor(){
+    
+    cont = 1;
+    [].forEach.call(cards, function(card){
+        if(card.classList.length < 2){
+            cont = cont - 1;
+            console.log(cont)
+        }
+    })
+
+    if(cont == 1){
+        if(pontos_jogador1 > pontos_jogador2){
+            qual_jogador.innerHTML = "vencedor: jogador 1";
+        }else if(pontos_jogador1 < pontos_jogador2){
+            qual_jogador.innerHTML = "vencedor: jogador 2";
+        }else if(pontos_jogador1 == pontos_jogador2){
+            qual_jogador.innerHTML = "empate";
+        }
+    }
+}
+
+
+
+////modal\\\\
+function open_modal(castro){
+    var modal = document.getElementById('modal');
+    modal.style.display = "block";
+    
+}
+
+  
+
+  
